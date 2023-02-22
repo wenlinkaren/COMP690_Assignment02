24ac160ee2f65080aa4f659418395ce9b5b96021
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-21 21:46:40 -0800

    The final edit

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -2,15 +2,15 @@ let number = parseInt(prompt("Enter a number between 1 and 100:"))
 
 if (number >= 1 && number <= 100) {
   if (number >= 90 && number <= 100) {
-    console.log("You received an A");
+    console.log("You received an A")
   } else if (number >= 80 && number <= 89) {
-    console.log("You received a B");
+    console.log("You received a B")
   } else if (number >= 70 && number <= 79) {
-    console.log("You received a C");
+    console.log("You received a C")
   } else if (number >= 60 && number <= 69) {
-    console.log("You received a D");
+    console.log("You received a D")
   } else {
-    console.log("You received an F");
+    console.log("You received an F")
   }
 } else {
   console.log("Only numbers between 1 and 100 are accepted.")
