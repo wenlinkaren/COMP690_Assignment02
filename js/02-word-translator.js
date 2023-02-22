6c466537accd2c0bc0bf18a6118c68dce93d08fb
Wenlin He <wenlinhe@Wenlins-MacBook-Pro.local>
2023-02-21 21:05:04 -0800

    The Hello Translator

---
+++ js/02-word-translator.js
@@ -0,0 +1,16 @@
+let languageCode = prompt("Enter language code (es, de, en, fr):")
+
+switch (languageCode) {
+  case "es":
+    console.log("Hola Mundo") // Spanish translation
+    brea
+  case "de":
+    console.log("Hallo Welt") // German translation
+    break
+  case "fr":
+    console.log("Bonjour le monde") // French translation
+    break
+  default:
+    console.log("Hello World") // Default to English
+    break
+}
